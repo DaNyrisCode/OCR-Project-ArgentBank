934b0360b34ebe637e7d691b25eef7a340006a84
DaNyrisCode <komakhinamarina@gmail.com>
2025-02-03 17:54:51 +0100

    mep editform

---
+++ Frontend/src/components/User/EditProfileUser.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+
+const EditProfileUser = ({ user, newUserName, setNewUserName, handleSubmit, handleCancel }) => {
+  return (
+    <form onSubmit={handleSubmit} className="edit-form">
+      <div className="input-wrapper">
+        <label htmlFor="userName">User name:</label>
+        <input
+          type="text"
+          id="userName"
+          value={newUserName}
+          onChange={(e) => setNewUserName(e.target.value)}
+        />
+      </div>
+      <div className="input-wrapper">
+        <label htmlFor="firstName">First name:</label>
+        <input type="text" id="firstName" value={user.firstName} readOnly />
+      </div>
+      <div className="input-wrapper">
+        <label htmlFor="lastName">Last name:</label>
+        <input type="text" id="lastName" value={user.lastName} readOnly />
+      </div>
+      <div className="button-group">
+        <button type="submit" className="save-button">Save</button>
+        <button type="button" className="cancel-button" onClick={handleCancel}>
+          Cancel
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default EditProfileUser;
--- Frontend/src/components/User/UserHeader.jsx
+++ Frontend/src/components/User/UserHeader.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { updateUserName } from "../../redux/slices/updateProfileSlice";
 import { fetchUserProfile } from "../../redux/slices/profileSlice";
+import EditProfileUser from "./EditProfileUser";
 
 const UserHeader = () => {
   const dispatch = useDispatch();
@@ -9,12 +10,9 @@ const UserHeader = () => {
   const loading = useSelector((state) => state.profile.loading);
   const updatedUser = useSelector((state) => state.updateProfile.updatedUser);
 
-  // État local pour afficher le formulaire
   const [isEditing, setIsEditing] = useState(false);
-  const [newUserName, setNewUserName] = useState(user?.userName || "");
+  const [newUserName, setNewUserName] = useState("");
 
-
-  // Recharge le profil après la mise à jour
   useEffect(() => {
     if (updatedUser) {
       dispatch(fetchUserProfile());
@@ -22,22 +20,26 @@ const UserHeader = () => {
     }
   }, [updatedUser, dispatch]);
 
+  useEffect(() => {
+    if (user) {
+      setNewUserName(user?.userName);
+    }
+  }, [user]);
+
   const handleEditClick = () => {
     setIsEditing(true);
   };
 
-  // Réinitialise l'utilisateur et annule la mise à jour
   const handleCancel = () => {
-    setNewUserName(user?.userName || "");
+    setNewUserName(user.userName);
     setIsEditing(false);
   };
 
-
-  // Met à jour le userName
   const handleSubmit = (e) => {
     e.preventDefault();
     if (newUserName.trim() !== "") {
       dispatch(updateUserName(newUserName));
+      setNewUserName("");
     }
   };
 
@@ -51,34 +53,20 @@ const UserHeader = () => {
             Welcome back<br />
             {user.firstName} {user.lastName}!
           </h1>
-          <h2> AkA {user.userName}</h2>
+          <h2>AkA {user.userName}</h2>
 
           {isEditing ? (
-            <form onSubmit={handleSubmit} className="edit-form">
-              <div className="input-wrapper">
-                <label htmlFor="userName">User name:</label>
-                <input
-                  type="text"
-                  id="userName"
-                  value={newUserName}
-                  onChange={(e) => setNewUserName(e.target.value)}
-                />
-              </div>
-              <div className="input-wrapper">
-                <label htmlFor="firstName">First name:</label>
-                <input type="text" id="firstName" value={user.firstName} readOnly />
-              </div>
-              <div className="input-wrapper">
-                <label htmlFor="lastName">Last name:</label>
-                <input type="text" id="lastName" value={user.lastName} readOnly />
-              </div>
-              <div className="button-group">
-                <button type="submit" className="save-button">Save</button>
-                <button type="button" className="cancel-button" onClick={handleCancel}>Cancel</button>
-              </div>
-            </form>
+            <EditProfileUser
+              user={user}
+              newUserName={newUserName}
+              setNewUserName={setNewUserName}
+              handleSubmit={handleSubmit}
+              handleCancel={handleCancel}
+            />
           ) : (
-            <button className="edit-button" onClick={handleEditClick}>Edit Name</button>
+            <button className="edit-button" onClick={handleEditClick}>
+              Edit Name
+            </button>
           )}
         </>
       ) : (
